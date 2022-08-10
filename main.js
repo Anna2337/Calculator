aafc5e2df91030a0bdc27bd3eaf15e8c3b4a9aee
Ann <79790703+Anna2337@users.noreply.github.com>
2022-08-10 12:01:20 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,89 @@
+let a = ''; // первое число
+let b = ''; // второе число
+let sign = ''; // знак операции
+let finish = ''; // результат
+
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const action = ['-', '+', '*', '%', '/'];
+
+const out = document.querySelector('.calc-screen p');
+
+function clearAll () {
+	a = '';
+	b = '';
+	sign = '';
+	finish = '';
+	out.textContent = 0;
+}
+
+document.querySelector('.ac').onclick = clearAll;
+
+document.querySelector('.buttons').onclick = (event) => {
+	if(!event.target.classList.contains('btn')) return;
+	if(event.target.classList.contains('ac')) return;
+
+	out.textContent = '';
+	
+	const key = event.target.textContent;
+
+	if (digit.includes(key)) {
+		if (b === '' && sign === '') {
+			a += key;
+			console.log (a, b, sign);
+			out.textContent = a;
+		}
+		else if (a!=='' && b!=='' && finish) {
+			b = key;
+			finish = false;
+			out.textContent = b;
+		}
+		else {
+			b +=key;
+			out.textContent = b;
+		}
+		console.log (a, b, sign);
+		return
+	}
+
+	if (action.includes(key)) {
+		sign = key;
+		out.textContent = sign;
+		console.log (a, b, sign);
+		return;
+	}
+
+	if (key === '=') {
+		if (b === '') b = a
+		switch (sign) {
+			case "+":
+				a = (+a) + (+b);
+				break;
+
+			case "-":
+				a = a - b;
+				break;
+
+			case "*":
+				a = a * b;
+				break;
+
+			case "%":
+				a = a % b;
+				break;
+
+			case "/":
+				if (b === '0') {
+					out.textContent = 'Ошибка';
+					a = '';
+					b = '';
+					sign = '';
+					return;
+				}
+				a = a / b;
+				break;
+		}
+		finish = true;
+		out.textContent = a;
+	}
+
+}
